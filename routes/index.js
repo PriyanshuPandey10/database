651aebea6f8b5712714255feb26b4b2a3ad03ecc
PriyanshuPandey10 <pandey.priyanshu10@gmail.com>
2024-06-17 11:35:31 +0530

    mongo db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
 
 /* GET home page. */
 router.get("/", function(req, res) {
-res.render('index', { title: 'Express' });
+    res.render("index");
 });
 
+router.get("/create", async function(req, res) {
+    
+        const usercreated = await userModel.create({
+            userName: "Priyanshu Pandey",
+            age: 21,
+            name: 'Rahul'
+        });
+        res.send(usercreated);
+
+});
 
 module.exports = router;
